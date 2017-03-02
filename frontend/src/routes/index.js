e2267089495a3d5c7ff2c11330bf9116d105ff9c
anand <asrini@mit.edu>
2017-03-01 18:25:51 -0800

    define routes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,16 +1,62 @@
+import request from 'request'
+
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/CoreLayout'
 import Home from './Home'
 
+const mirroredRoute = ({path, propKey, componentPath, childRoutes}) => (
+  (store) => ({
+    path,
+    childRoutes,
+    getComponent (nextState, cb) {
+    /*  Webpack - use 'require.ensure' to create a split point
+        and embed an async module loader (jsonp) when bundling   */
+    require.ensure([], (require) => {
+      /*  Webpack - use require callback to define
+          dependencies for bundling   */
+      const Component = require(componentPath).default
+
+      /*  Return getComponent   */
+      const url = nextState.location.path // TODO this could be wrong
+      request
+        .get(url)
+        .on('response', (response) => {
+          if (response.statusCode == 200) {
+            const props = {}
+            props[propKey] = JSON.parse(response.body)
+            cb(null, <Component {...props} />)
+          } else {
+            cb(null, <RedBox text={`failed to reach ${url}`} />)
+          }
+        })
+        .
+
+    /* Webpack named bundle   */
+    }, 'counter')
+  }
+  })
+)
+
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
+
+export const mirroredRoute({
+  path: '/',
+  propKey: 'items',
+  componentPath: '../components/Feed',
+  childRoutes: [
+    mirroredRoute({
+      path: ''
+    })
+  ]
+})
 
 export const createRoutes = (store) => ({
   path        : '/',
   component   : CoreLayout,
-  indexRoute  : Home,
+  indexRoute  : {component: Feed},
   childRoutes : [
-    CounterRoute(store)
+
   ]
 })
 
